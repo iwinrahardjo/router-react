95c384d26b909188827225de33552e06780aa867
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-10 11:04:20 +0700

    start naming the components

---
+++ src/components/Body/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  render() {
+    return <Content />;
+  }
+}
+
+export default Body;
---
+++ src/components/Content/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  render() {
+    return (
+        `coba`
+        `apaya`;
+    )
+  }
+}
+
+export default Content;
